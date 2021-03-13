af89116a83ced88598eb37dbe5ce27166524e9f4
Viktor <viktor.tholen@gmail.com>
2021-03-13 15:21:52 +0100

    init

---
+++ src/components/Chart.vue
@@ -0,0 +1,33 @@
+<script>
+import { Bubble} from 'vue-chartjs'
+
+//TODO: Color code indexChange (blue -> red scale)
+
+
+export default {
+    extends: Bubble,
+    name: "bubble-chart",
+    props: ['chartData', 'options'],
+    mounted(){
+        console.log("comp: ", this.chartData[0]);
+        let parsedData = this.chartData.map( (d) => ({x: d.index, y: d.responseRate}));
+        delete parsedData[0];
+        console.log(this.options);
+
+        this.renderChart({
+            // label: "Scatter plot",
+            datasets: [{
+                label: "Data",
+                data: parsedData,
+                radius: this.chartData.map((d) =>{return d.employees;} ),
+                backgroundColor: "steelblue"
+            }]},
+            this.options
+        
+        );
+    }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Container.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <h2>Visualization</h2>
+    <div class="center">
+      <Chart v-bind:chartData="chartData" v-bind:options="options" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Chart from "./Chart.vue";
+import employee_data from "../data/data.json";
+
+export default {
+  name: "Container",
+  data: () => ({
+    chartData: employee_data,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          //   left: 50,
+          //   right: 50
+        },
+      },
+    },
+  }),
+  components: {
+    Chart,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  /* height: 50%; */
+}
+.center {
+  margin: auto;
+  max-width: 50%;
+  max-height: 50%;
+  min-height: 50%;
+}
+</style>
